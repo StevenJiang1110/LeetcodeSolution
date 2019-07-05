d043fb110a67a05085e1af91ef58a2961288df1e
stevenjiang <stevenjiang1110@gmail.com>
2019-07-05 12:03:40 +0800

    problem 34

--- src/problem34/SearchRange.java
+++ src/problem34/SearchRange.java
@@ -8,7 +8,7 @@ public class SearchRange {
     public int[] searchRange(int[] nums, int target) {
         int index = Arrays.binarySearch(nums, target);
         int[] res = {-1, -1};
-        if(index < -1) return res;
+        if(index < 0) return res;
         for(int i = index; i>=0; i--){
             if(nums[i] == target){
                 res[0] = i;
@@ -31,13 +31,8 @@ public class SearchRange {
         int[] nums = {1,2,3};
         int target = 2;
         int[] res = new SearchRange().searchRange(nums, target);
-
-        String[] strings = {"aa", "bb", "cc"};
-
-        List reslist = Arrays.asList(strings);
-        reslist.forEach(System.out::println);
-        ArrayList<?> arrayList = new ArrayList<>();
-        Object obj;
-        String s;
+        for (int i = 0, len = res.length; i < len; i++) {
+            System.out.println(res[i]);
+        }
     }
 }
